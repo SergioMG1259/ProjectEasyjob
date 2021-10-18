82d5345da90db129adf5942d7161576f02744689
Symphony <49285362+symphony701@users.noreply.github.com>
2021-10-18 14:26:11 -0500

    List postulant style ready

--- src/views/Announcement/View-Announcement.vue
+++ src/views/Announcement/View-Announcement.vue
@@ -128,3 +128,20 @@ export default {
 </script>
 
 
+<style scoped>
+.data-announcement{
+  margin-left: 20px;
+  margin-top: 10px;
+  margin-right: 10px;
+}
+.scroll{
+  max-height: 350px;
+  overflow-y: auto;
+  overflow-y: scroll;
+}
+.card-an{
+  margin-bottom: 20px;
+  border: solid 1px purple;
+  border-radius: 10px;
+}
+</style>
