673ddc533b9c49c6d9b6e210ac17e6f77e2e88e7
Symphony <49285362+symphony701@users.noreply.github.com>
2021-10-18 14:25:48 -0500

    Update View-Announcement.vue

--- src/views/Announcement/View-Announcement.vue
+++ src/views/Announcement/View-Announcement.vue
@@ -128,20 +128,3 @@ export default {
 </script>
 
 
-<style scoped>
-.data-announcement{
-  margin-left: 20px;
-  margin-top: 10px;
-  margin-right: 10px;
-}
-.scroll{
-  max-height: 350px;
-  overflow-y: auto;
-  overflow-y: scroll;
-}
-.card-an{
-  margin-bottom: 20px;
-  border: solid 1px purple;
-  border-radius: 10px;
-}
-</style>
