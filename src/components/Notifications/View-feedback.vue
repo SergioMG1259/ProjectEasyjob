8ccc1d12cf1d5f923130e9bb27aab978502a1ad8
alesZZ <81696641+alesZZ@users.noreply.github.com>
2021-10-19 11:15:13 -0500

    view feedback done

--- src/components/Notifications/View-feedback.vue
+++ src/components/Notifications/View-feedback.vue
@@ -1,1 +1,34 @@
+<template>
+  <v-dialog width="600px" v-model="dialog" persistent>
+    <v-card>
+      <v-card-text class="form-edit">
+        {{feedback_text}}
+      </v-card-text>
+      <v-card-actions class="form-edit">
+        <v-btn @click="close_this_dialog">Cerrar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
 
+<script>
+export default {
+  name: "View-feedback",
+  props:["dialog","feedback"],
+  data:()=>({
+    feedback_text:"s"
+  }),
+  methods:{
+    close_this_dialog(){
+      this.$emit('close_feedback',false)
+    }
+  },
+  mounted() {
+    this.feedback_text=this.feedback
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
