896874c778bffccfda553c02c00f36717120a9e1
alesz <alexlitanoliza@hotmail.com>
2021-10-19 11:26:14 -0500

    view feedback v2 done

--- src/components/Notifications/View-feedback.vue
+++ src/components/Notifications/View-feedback.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- Esto es para que el postulante vea el feedback -->
+  <!-- Esto es para que un postulante vea el feedback -->
   <v-dialog width="600px" v-model="dialog" persistent>
     <v-card>
       <v-card-text class="form-edit">
